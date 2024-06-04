bccf76009d0e50ca3ee25f46ea9edfe3e6303702
thinhvip1 <thinhk420@gmail.com>
2024-06-04 23:46:19 +0700

    ex math in day 10th

---
+++ math/1335A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        // if(n%2==0)
+        //     cout << n/2-1 << endl;
+        // else 
+        //     cout << n/2 << endl;
+        cout << (n-1)/2 << endl;
+    }
+}
